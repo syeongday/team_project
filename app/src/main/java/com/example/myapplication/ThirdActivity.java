60df6b7138918fb08f5ac3610d2cdae090153e38
svc <syeongni@naver.com>
2023-12-04 22:45:35 +0900

    commit

--- app/src/main/java/com/example/myapplication/ThirdActivity.java
+++ app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -1,12 +1,32 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends Activity {
+    Button bt1;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third);
         setTitle("Third 액티비티");
+
+        bt1 = findViewById(R.id.bt1);
+        bt1.setOnTouchListener(new View.OnTouchListener()
+        {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    bt1.setBackgroundColor(Color.rgb(255, 204, 204));
+                }
+                return false;
+            }
+        });
     }
 }
